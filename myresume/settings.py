ebe2e47a6dddf3d703873c8c0da6f8b7a2dc20fa
Venkat <sai.venkat222@gmail.com>
2018-09-15 23:57:34 +0530

    initial commit

--- myresume/settings.py
+++ myresume/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myresume',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -98,7 +99,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
--- myresume/wsgi.py
+++ myresume/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for myresume project.
+import  os
+import sys
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+path ='/home/venkatpodili/myresume'
+if path not in sys.path:
+    sys.path.append(path)
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
-import os
+os.environ['Django_SETTINGS_MODULE'] = 'myresume.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myresume.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
